76c6713a43fb21da88c4992f68ca74b9ac6650dc
Mark Fay <fay.mark@gmail.com>
2013-11-12 22:53:37 -0500

    removed add user route (in jake file now)

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -9,13 +9,3 @@ exports.list = function(req, res){
 exports.login = function(req, res) {
 	res.render('login');
 }
-
-exports.admin = function(req, res) {
-	var user = new User();
-	user.name = req.query.name;
-	user.email = req.query.email;
-	user.password = req.query.password;
-	user.save(function(err, something){
-		res.redirect('/');
-	});
-}
--- config/routes.js
+++ config/routes.js
@@ -5,7 +5,6 @@ var auth = require('../app/middleware/authorization');
 module.exports = function(app, passport) {
 	app.get('/', routes.index);
 	app.get('/users', auth.requiresLogin, user.list);
-	app.get('/user/new', user.admin);
 	app.get('/user/login', user.login);
 	app.post('/user/session', passport.authenticate('local', {
 		successRedirect: '/users', failureRedirect: '/user/login' }));
